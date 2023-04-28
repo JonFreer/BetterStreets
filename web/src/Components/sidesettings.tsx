a40b6d04b0918603de7605a53a9ab7e847392df7
George Kaye <georgejkaye@gmail.com>
2023-04-28 17:45:49 +0100

    fix: change sidebar close button to cross

--- web/src/Components/sidesettings.tsx
+++ web/src/Components/sidesettings.tsx
@@ -1,7 +1,5 @@
-import React, { useRef, useEffect, useState, ChangeEvent } from 'react';
 import styles from '../css/settings.module.css';
-import {GrClose} from "react-icons/gr"
-import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiOutlineCloseCircle} from 'react-icons/ai'
 function SideBarSettings(props: { open:boolean, closeCallback:any, settings:any, updateCallback:any}) {
     if(!props.open){
         document.getElementById('settings')?.classList.remove(styles.open);
@@ -25,23 +23,23 @@ function SideBarSettings(props: { open:boolean, closeCallback:any, settings:any,
         const clonedSettings = { ...props.settings };
         clonedSettings.completed = !clonedSettings.completed;
         props.updateCallback(clonedSettings)
-    })  
+    })
 
     const updateIncomplete =(()=>{
         const clonedSettings = { ...props.settings };
         clonedSettings.incomplete = !clonedSettings.incomplete;
         props.updateCallback(clonedSettings)
-    })  
+    })
 
     const updateUnclassified =(()=>{
         const clonedSettings = { ...props.settings };
         clonedSettings.unclassified = !clonedSettings.unclassified;
         props.updateCallback(clonedSettings)
-    })  
+    })
 
     return(<div id="settings" className={styles.sidebar}>
         <div className={styles.sidebar_title}>Filter</div>
-        <div className={styles.sidebar_check} >    
+        <div className={styles.sidebar_check} >
                 <input type="checkbox" id="scales" name="scales" checked={props.settings.completed} onChange={()=>{
                      const clonedSettings = { ...props.settings };
                      clonedSettings.completed = !clonedSettings.completed;
@@ -50,7 +48,7 @@ function SideBarSettings(props: { open:boolean, closeCallback:any, settings:any,
                     }/>
                 <label className={styles.sidebar_label} onClick={()=>{updateCompleted()}}>Completed</label>
         </div>
-        <div className={styles.sidebar_check} >    
+        <div className={styles.sidebar_check} >
                 <input type="checkbox" id="scales" name="scales" checked={props.settings.incomplete} onChange={()=>{
                      const clonedSettings = { ...props.settings };
                      clonedSettings.incomplete = !clonedSettings.incomplete;
@@ -58,7 +56,7 @@ function SideBarSettings(props: { open:boolean, closeCallback:any, settings:any,
                     }}/>
                 <label className={styles.sidebar_label} onClick={()=>{updateIncomplete()}}>Incomplete</label>
         </div>
-        <div className={styles.sidebar_check} >    
+        <div className={styles.sidebar_check} >
                 <input type="checkbox" id="scales" name="scales" checked={props.settings.unclassified}onChange={()=>{
                      const clonedSettings = { ...props.settings };
                      clonedSettings.unclassified = !clonedSettings.unclassified;
@@ -66,8 +64,7 @@ function SideBarSettings(props: { open:boolean, closeCallback:any, settings:any,
                     }}/>
                 <label className={styles.sidebar_label} onClick={()=>{updateUnclassified()}} >Unclassified</label>
         </div>
-
-        <button onClick={()=>{props.closeCallback()}} className={styles.close}><GiHamburgerMenu></GiHamburgerMenu></button>
+        <button onClick={()=>{props.closeCallback()}} className={styles.close}><AiOutlineCloseCircle /></button>
     </div>)
 
 }
